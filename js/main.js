fc7b8e29b83ef61e96ab97a13e32ed427a31ebea
Royshacked <roy.shacked@mail.huji.ac.il>
2024-02-01 15:15:55 +0200

    Limit ballsize growth

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ function onBallClick(elBall) {
     var ballSize = +elBall.innerText
     ballSize += 50
 
+    if (ballSize > 400) ballSize = 100
+    
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.innerText = ballSize
